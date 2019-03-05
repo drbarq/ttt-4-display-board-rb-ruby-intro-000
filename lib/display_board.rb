0694acea84f4ccbcc3cd1930e3d95d57f855bd78
Joe Tustin <j.tustin@gmail.com>
2019-03-05 18:55:35 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,3 +7,10 @@ def display_board(board)
   puts "-----------"
   puts ` #{board[6]} | #{board[7]} | #{board[8]} `
 end
+
+
+puts row[0]
+puts "-----------"
+puts ` #{board[3]} | #{board[4]} | #{board[5]} `
+puts "-----------"
+puts ` #{board[6]} | #{board[7]} | #{board[8]} `
