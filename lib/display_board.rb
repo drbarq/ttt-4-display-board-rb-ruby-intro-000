061f7a2c06eb23ab74c6a06b78cf76ea51c10fd6
Joe Tustin <j.tustin@gmail.com>
2019-03-05 19:07:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,17 +6,17 @@
 
 board = [" "," "," "," "," "," "," "," "," "]
 
-rows[0] = ` "#{board[0]}" | "#{board[1]}" | "#{board[2]}" `
-rows[1] =  "-----------"
-rows[2] = ` "#{board[3]}" | "#{board[4]}" | "#{board[5]}" `
-rows[3] = "-----------"
-rows[4] = ` "#{board[6]}" | "#{board[7]}" | "#{board[8]}" `
+row[0] = ` "#{board[0]}" | "#{board[1]}" | "#{board[2]}" `
+row[1] =  "-----------"
+row[2] = ` "#{board[3]}" | "#{board[4]}" | "#{board[5]}" `
+row[3] = "-----------"
+row[4] = ` "#{board[6]}" | "#{board[7]}" | "#{board[8]}" `
 
 
 def display_board(board)
-  puts rows[0]
-  puts rows[1]
-  puts rows[2]
-  puts rows[3]
-  puts rows[4]
+  puts row[0]
+  puts row[1]
+  puts row[2]
+  puts row[3]
+  puts row[4]
 end
